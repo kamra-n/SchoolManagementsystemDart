106fdaf334f5a5df3591dcad17e4476cca7138d6
kamra-n <ikamihere@gmail.com>
2024-05-12 17:16:23 +0500

    wip

--- Admin.dart
+++ Admin.dart
@@ -11,13 +11,27 @@ class Admin {
 
     GlobalList.TeacherRecord.add(teacherRecord);
 
-    print('Teacher list isssssss ========> ${GlobalList.TeacherRecord}');
+    // print('Teacher list isssssss ========> ${GlobalList.TeacherRecord}');
   }
 
-  static updateTeacher(
-      String name, int age, List subject, List courses, num salary) {}
+  static updateTeacher(int id, String name, int age, List subject, num salary) {
+    for (var i = 0; i < GlobalList.TeacherRecord.length; i++) {
+      if (i == id) {
+        GlobalList.TeacherRecord[i]['Name'] = name;
+        GlobalList.TeacherRecord[i]['Age'] = age;
+        GlobalList.TeacherRecord[i]['Subjects'] = subject;
+        GlobalList.TeacherRecord[i]['Salary'] = salary;
+        print('Teacher Record Update');
+        print(GlobalList.TeacherRecord[i]);
+      } else {
+        print('No Teacher Found on specific index');
+      }
+    }
+  }
 
-  static delteTeacher(int id) {}
+  static delteTeacher(int id) {
+    print('agr ban jata hai tw ur qualified for a job for software');
+  }
 
   static addStudent(
       String name, int age, List enrollSubjects, int rollnum, int stdclass) {}
--- main.dart
+++ main.dart
@@ -38,44 +38,70 @@ void adminlogin() {
   String? password = stdin.readLineSync();
 
   if (email == GlobalList.adminEmail && password == GlobalList.adminPassword) {
-    print('Welcome to Admin Portal');
-    print('Select Options to do');
-    print(
-        ' 1- Add Teacher \n 2- Update Teacher \n 3- Delete Teacher \n 4- Add Student \n 5- Update Student \n 6- Delete Student');
-    String? userSelection = stdin.readLineSync();
-    switch (userSelection) {
-      case '1':
-        int count = 1;
-        List subjects = [];
-        print('Add Teacher name');
-        String? teacherName = stdin.readLineSync();
-        print('Add Teacher Age');
-        int teacherAge = int.parse(stdin.readLineSync()!);
-        print('Add Teacher Salary');
-        num? teacherSalary = int.parse(stdin.readLineSync()!);
-        print('Add Teacher name');
-        while (count < 6) {
-          print('Add Teacher Subjects number ${count}');
-          String? subjectname = stdin.readLineSync();
-          subjects.add(subjectname);
-          count++;
-        }
-        Admin.addTeacher(teacherName!, teacherAge, subjects, teacherSalary);
-        break;
-      case '2':
-        print('hello case 2');
-        break;
-      case '3':
-        print('hello case 3');
-        break;
-      case '4':
-        print('See you later byee');
-        break;
-      default:
-        print('Please select correct option');
-        mainFunction();
-    }
+    adminPortal();
   } else {
     print('Incorrect email password');
+  }
+}
+
+void adminPortal() {
+  print('Welcome to Admin Portal');
+  print('Select Options to do');
+  print(
+      ' 1- Add Teacher \n 2- Update Teacher \n 3- Delete Teacher \n 4- Add Student \n 5- Update Student \n 6- Delete Student');
+  String? userSelection = stdin.readLineSync();
+  switch (userSelection) {
+    case '1':
+      int count = 1;
+      List subjects = [];
+      print('Add Teacher name');
+      String? teacherName = stdin.readLineSync();
+      print('Add Teacher Age');
+      int teacherAge = int.parse(stdin.readLineSync()!);
+      print('Add Teacher Salary');
+      num? teacherSalary = int.parse(stdin.readLineSync()!);
+      print('Add Teacher name');
+      while (count < 6) {
+        print('Add Teacher Subjects number ${count}');
+        String? subjectname = stdin.readLineSync();
+        subjects.add(subjectname);
+        count++;
+      }
+      Admin.addTeacher(teacherName!, teacherAge, subjects, teacherSalary);
+      adminPortal();
+      break;
+    case '2':
+      int count = 1;
+      List subjects = [];
+      print('updated id');
+      int id = int.parse(stdin.readLineSync()!);
+      print('updated Teacher name');
+      String? teacherName = stdin.readLineSync();
+      print('updated Teacher Age');
+      int teacherAge = int.parse(stdin.readLineSync()!);
+      print('updated Teacher Salary');
+      num? teacherSalary = int.parse(stdin.readLineSync()!);
+      while (count < 6) {
+        print('updated Teacher Subjects number ${count}');
+        String? subjectname = stdin.readLineSync();
+        subjects.add(subjectname);
+        count++;
+      }
+      Admin.updateTeacher(
+          id, teacherName!, teacherAge, subjects, teacherSalary);
+      adminPortal();
+
+      break;
+    case '3':
+      print('hello case 3');
+      adminPortal();
+
+      break;
+    case '4':
+      print('See you later byee');
+      break;
+    default:
+      print('Please select correct option');
+      mainFunction();
   }
 }
